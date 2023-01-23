f6c55d21689cd1920710870c7c7841411ebbc879
Etta <etta.lioness@hotmail.com>
2023-01-23 19:18:25 +1100

    updated categories
    -Added more choices to CATEGORY variables.
    -I made migrations and checked it is functioning as expected in insomnia when creating a new project with a user token. Code 201-Created, all good!

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -5,8 +5,26 @@ from django.contrib.auth import get_user_model #Django will find what model you 
 
 User = get_user_model()
 CATEGORIES = (
-    ("Engineering", "Engineering"),
-    ("Chemistry", "Chemistry"),
+    ("Eng", "Engineering"),
+    ("Chem", "Chemistry"),
+    ("Bio", "Biology"),
+    ("Math", "Mathematics"),
+    ("Social Sci", "Social Sciences"),
+    ("Ecom", "Economic"),
+    ("Data Sc", "Data Science"),
+    ("Comp Sci", "Computer Science"),
+    ("Ecology", "Ecology"),
+    ("Physics", "Physics"),
+    ("Materials Sc", "Material Science"),
+    ("Earth Sci", "Earth Science"), 
+    ("Edu", "Education"),
+    ("Paleo", "Paleontology"),
+    ("Med", "Medicine"),
+    ("Neuro", "Neuroscience"),
+    ("Psych", "Pschycology"),
+    ("Anthropology", "Anthropology"),
+    ("Art and Design", "Art and Design"),
+    ("Political Sc", "Political Science")
     
 )
 class Project(models.Model):
@@ -17,8 +35,11 @@ class Project(models.Model):
     video=models.URLField()
     is_open=models.BooleanField()
     date_created=models.DateTimeField(auto_now_add=True) 
-    category = models.CharField(max_length=200, null=True, choices= CATEGORIES)
     #(above)tells django when created to add at current time
+    category = models.CharField(max_length=200, null=True, choices= CATEGORIES)
+    #choices-A sequence of 2-tuples to use as choices for this field. 
+    # The default form widget will be a select box instead of the standard limited to the choices given.
+    # The first element in tuple is value stored database. The second displayed by the field’s form widget.
     owner=models.ForeignKey( #need to change this (above) to foreignKey(other table primary key),user id
         User,
         on_delete=models.CASCADE,
