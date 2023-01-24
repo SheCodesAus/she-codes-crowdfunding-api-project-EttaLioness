0737854eaf72025b5aa22ed082ef024fa04834e8
Etta <etta.lioness@hotmail.com>
2023-01-24 13:01:21 +1100

    user field updates
    added profile image, bio, first/last name, date joined, qualifications, and affiliate fields

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    # bio = model.TextField()
+    image = models.URLField(default = "https://i.postimg.cc/rm82XhCm/default-profile-image.png") #added
+    #https://pypi.org/project/django-default-imagefield/
+    # if deploying to a production environment, you must run the ./manage.py collectstatic command to copy
+    #  the image files included in the package to the static file serving path
+    bio = models.TextField(null = True) #added
+    qualifications = models.TextField(null = True) #added
+    affiliate = models.CharField(max_length=200, null = True) #added
 
     def __str__(self):
         return self.username
--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.utils import timezone #added
 
 class CustomUserSerializer(serializers.Serializer):
 
     id = serializers.ReadOnlyField()
+    first_name = serializers.CharField(max_length=200) #added
+    last_name = serializers.CharField(max_length=200) #added
     username = serializers.CharField(max_length=200)
+    date_joined = serializers.DateTimeField(default= timezone.now) #added
     email = serializers.EmailField()
+    image = serializers.URLField(default = "https://i.postimg.cc/rm82XhCm/default-profile-image.png") #added
+    bio = serializers.CharField(max_length=None, default = None) #added
+    qualifications = serializers.CharField(max_length=None, default = None) #added
+    affiliate = serializers.CharField(max_length=200) #added
     password = serializers.CharField(write_only = True)
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
+
+# class CustomUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['id', 'first_name', 'last_name', 'email', 'username', 'password',]
+#         read_only_fields = ['id', 'email', 'username',]
+
+        # def create(self, validated_data):
+        #     return CustomUser.objects.create_user(**validated_data)
